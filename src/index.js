6fc599e2705160dada8a5ba517877cea2ff2096f
Mariana Shydlovska <marjanka77777@gmail.com>
2023-04-19 00:55:23 +0200

    implement: units conversion

--- src/index.js
+++ src/index.js
@@ -21,7 +21,8 @@ function handleSubmit (event) {
 	search(city);
 };
 function showWeather(response) {
-	document.querySelector("#current-temperature").innerHTML = Math.round(response.data.main.temp);
+	celsiusTemperature = response.data.main.temp;
+	document.querySelector("#main-temperature").innerHTML = Math.round(celsiusTemperature);
 	document.querySelector("#humidity").innerHTML = Math.round(response.data.main.humidity);
 	document.querySelector("#wind").innerHTML = Math.round(response.data.wind.speed);
 	document.querySelector("#sky").innerHTML = response.data.weather[0].description;
@@ -41,6 +42,26 @@ let searchButton = document.querySelector("#search-button");
 searchButton.addEventListener("click", handleSubmit);
 search("New York");
 
+function showFahrenheitTemperature (event) {
+	event.preventDefault();
+	celsiusLink.classList.remove("active");
+	fahrenheitLink.classList.add("active");
+	let fahrenheitTemperature = (celsiusTemperature * 9) / 5 +32;
+	document.querySelector("#main-temperature").innerHTML = Math.round(fahrenheitTemperature);
+}
+function showCelsiusTemperature (event) {
+	event.preventDefault();
+	celsiusLink.classList.add("active");
+	fahrenheitLink.classList.remove("active");
+	document.querySelector("#main-temperature").innerHTML = Math.round(celsiusTemperature);
+}
+let celsiusTemperature = null;
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", showFahrenheitTemperature); 
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", showCelsiusTemperature); 
 
 
 
